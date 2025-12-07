3c7f73ba3149b1c02eefd227cad8bf7d45f848e0
Khoi Pham <khoipng@gmail.com>
2025-12-07 21:10:19 +0700

    update

---
+++ _site/assets/js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function(){
+	var yearEl = document.getElementById("year");
+	if(yearEl){
+		yearEl.textContent = new Date().getFullYear();
+	}
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+	var yearEl = document.getElementById("year");
+	if(yearEl){
+		yearEl.textContent = new Date().getFullYear();
+	}
+});
+
+
