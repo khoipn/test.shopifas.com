afea176e71120964f8dca40f52adde02e1f544e1
Khoi Pham <khoipng@gmail.com>
2025-12-07 16:34:36 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,11 @@ document.addEventListener("DOMContentLoaded", function(){
 	}
 });
 
+document.addEventListener("DOMContentLoaded", function(){
+	var yearEl = document.getElementById("year");
+	if(yearEl){
+		yearEl.textContent = new Date().getFullYear();
+	}
+});
+
 
